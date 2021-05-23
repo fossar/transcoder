de61ecdf442712862c0f7f15fc795d99c01ad26a
Jan Tojnar <jtojnar@gmail.com>
2021-05-23 05:29:42 +0200

    flake: Add a way to quickly disable iconv or mbstring

--- flake.nix
+++ flake.nix
@@ -16,6 +16,11 @@
 
       # By default, we use the default PHP version from Nixpkgs.
       matrix.phpPackage = "php";
+      # Allow easily removing some extensions for testing.
+      matrix.forbiddenExtensionNames = [
+        # "iconv"
+        # "mbstring"
+      ];
     in
       let
         # We only support a single platform at the moment,
@@ -26,14 +31,23 @@
         pkgs = nixpkgs.legacyPackages.${system};
 
         # Create a PHP package from the selected PHP package.
-        php = phps.packages.${system}.${matrix.phpPackage};
+        phpBase = phps.packages.${system}.${matrix.phpPackage};
+        php =
+          phpBase.withExtensions
+            ({ all, enabled }:
+              let
+                # Get the forbidden extensions by name.
+                forbiddenExtensions = pkgs.lib.attrVals matrix.forbiddenExtensionNames all;
+              in
+              builtins.filter (ext: !builtins.elem ext forbiddenExtensions) enabled
+            );
       in {
         # Expose shell environment for development.
         devShell.${system} = pkgs.mkShell {
           nativeBuildInputs = [
             # Composer and PHP.
             php
-            php.packages.composer
+            phpBase.packages.composer
           ];
         };
       };
